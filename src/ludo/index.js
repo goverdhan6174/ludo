e4605bf6f63c13b8022bbb108cf0d2d820416c39
Goverdhan Singh <goverdhansingh669@gmail.com>
2021-02-12 23:16:35 +0530

    remove depen

--- src/ludo/index.js
+++ src/ludo/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
-import socketIOClient from "socket.io-client";
+// import socketIOClient from "socket.io-client";
 
 import LudoApp from "./LudoApp";
 import LudoHomePage from "./ludoHomePage";
